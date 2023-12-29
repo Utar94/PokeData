c82eaea797161e1c8315ab2094822edbbd1a8b3d
Francis Pion <francispion@hotmail.com>
2023-12-28 23:00:33 -0500

    Implemented Species import persistence layer.

---
+++ src/PokeData.EntityFrameworkCore.Relational/Handlers/Resources/ResourceSavedEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PokeData.Domain.Resources.Events;
+using PokeData.EntityFrameworkCore.Relational.Entities;
+
+namespace PokeData.EntityFrameworkCore.Relational.Handlers.Resources;
+
+internal class ResourceSavedEventHandler : INotificationHandler<ResourceSavedEvent>
+{
+  private readonly PokemonContext _context;
+
+  public ResourceSavedEventHandler(PokemonContext context)
+  {
+    _context = context;
+  }
+
+  public async Task Handle(ResourceSavedEvent @event, CancellationToken cancellationToken)
+  {
+    ResourceEntity? entity = await _context.Resources
+      .SingleOrDefaultAsync(x => x.AggregateId == @event.AggregateId.Value, cancellationToken);
+
+    if (entity == null)
+    {
+      entity = new(@event);
+      _context.Resources.Add(entity);
+    }
+    else
+    {
+      entity.Save(@event);
+    }
+
+    await _context.SaveChangesAsync(cancellationToken);
+  }
+}
---
+++ src/PokeData.EntityFrameworkCore.Relational/Repositories/ResourceRepository.cs
@@ -0,0 +1,50 @@
+﻿using Logitar.EventSourcing;
+using Logitar.EventSourcing.EntityFrameworkCore.Relational;
+using Logitar.EventSourcing.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using PokeData.Domain.Resources;
+using PokeData.EntityFrameworkCore.Relational.Entities;
+
+namespace PokeData.EntityFrameworkCore.Relational.Repositories;
+
+internal class ResourceRepository : Logitar.EventSourcing.EntityFrameworkCore.Relational.AggregateRepository, IResourceRepository
+{
+  private readonly DbSet<ResourceEntity> _resources;
+
+  public ResourceRepository(IEventBus eventBus, EventContext eventContext, IEventSerializer eventSerializer, PokemonContext pokemonContext)
+    : base(eventBus, eventContext, eventSerializer)
+  {
+    _resources = pokemonContext.Resources;
+  }
+
+  public async Task<ResourceAggregate?> LoadAsync<T>(string identifier, CancellationToken cancellationToken)
+    => await LoadAsync(ResourceId.Create<T>(identifier), cancellationToken);
+  public async Task<ResourceAggregate?> LoadAsync(ResourceId id, CancellationToken cancellationToken)
+    => await LoadAsync<ResourceAggregate>(id.AggregateId, cancellationToken);
+  public async Task<ResourceAggregate?> LoadAsync(SourceUnit source, CancellationToken cancellationToken)
+  {
+    string sourceNormalized = source.Value.Trim().ToUpper();
+
+    ResourceEntity? entity = await _resources.AsNoTracking()
+      .SingleOrDefaultAsync(x => x.SourceNormalized == sourceNormalized, cancellationToken); // TODO(fpion): optimize query using raw SQL
+    if (entity == null)
+    {
+      return null;
+    }
+
+    AggregateId id = new(entity.AggregateId);
+
+    return await LoadAsync<ResourceAggregate>(id, cancellationToken);
+  }
+
+  public async Task<IEnumerable<ResourceAggregate>> LoadAsync(IEnumerable<ResourceId> ids, CancellationToken cancellationToken)
+  {
+    AggregateId[] aggregateIds = ids.Select(id => id.AggregateId).Distinct().ToArray();
+    return await LoadAsync<ResourceAggregate>(aggregateIds, cancellationToken);
+  }
+
+  public async Task SaveAsync(ResourceAggregate resource, CancellationToken cancellationToken)
+    => await base.SaveAsync(resource, cancellationToken);
+  public async Task SaveAsync(IEnumerable<ResourceAggregate> resources, CancellationToken cancellationToken)
+    => await base.SaveAsync(resources, cancellationToken);
+}
