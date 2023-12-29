47fcda0f64f55874789897758607590a4867e8c3
Francis Pion <francispion@hotmail.com>
2023-12-29 10:52:49 -0500

    Removed synchronization code.

--- src/PokeData.EntityFrameworkCore.Relational/Entities/RegionEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PokeData.EntityFrameworkCore.Relational.Entities;
-
-internal class RegionEntity // TODO(fpion): private access
-{
-  public int RegionId { get; set; }
-
-  public string UniqueName { get; set; } = string.Empty;
-  public string? DisplayName { get; set; }
-}
